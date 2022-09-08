7e0cf80a89ab808a3d09cc71bad920ae10807a0b
Malvino-creator <malvin.opondo@student.moringaschool.com>
2022-09-08 18:06:38 +0300

    feat(script.js): modifies the javascript file to add functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+var object={
+    balance: 100,
+    income:10000,
+    expense: 200,
+    transactions: [
+        {item: 'Salary', type:'Income', value: '10000'},
+        {item: 'Internet', type:'Expense', value: '10000'},
+        {item: 'Buy Groceries', type:'Expense', value: '10000'},
+        
+    ]
+}
+var balanceEl= document.querySelector('#balance');
+var incomeEl= document.querySelector('#income');
+var expenseEl= document.querySelector('#object-expense');
+
+function getCalculations() {
+    balance.El.innerHTML= 
+}
